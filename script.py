6a61a73759c6a75a6914b0f9fefa3843c7001d1d
mandar-01 <bhaleraomandar01@gmail.com>
2018-08-29 23:48:57 +0530

    removed comments

--- script.py
+++ script.py
@@ -11,10 +11,10 @@ from sklearn.preprocessing import MinMaxScaler
 df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
 print('Loaded data from the Kaggle repository')
 
-# Sort DataFrame by date
+
 df = df.sort_values('Date')
 
-# Double check the result
+
 df.head()
 
 plt.figure(figsize = (18,9))
@@ -24,7 +24,7 @@ plt.xlabel('Date',fontsize=18)
 plt.ylabel('Mid Price',fontsize=18)
 plt.show()
 
-# First calculate the mid prices from the highest and lowest
+
 high_prices = df.loc[:,'High'].as_matrix()
 low_prices = df.loc[:,'Low'].as_matrix()
 mid_prices = (high_prices+low_prices)/2.0
@@ -32,37 +32,34 @@ mid_prices = (high_prices+low_prices)/2.0
 train_data = mid_prices[:11000]
 test_data = mid_prices[11000:]
 
-# Scale the data to be between 0 and 1
-# When scaling remember! You normalize both test and train data with respect to training data
-# Because you are not supposed to have access to test data
 scaler = MinMaxScaler()
 train_data = train_data.reshape(-1,1)
 test_data = test_data.reshape(-1,1)
 
-# Train the Scaler with training data and smooth data
+
 smoothing_window_size = 2500
 for di in range(0,10000,smoothing_window_size):
     scaler.fit(train_data[di:di+smoothing_window_size,:])
     train_data[di:di+smoothing_window_size,:] = scaler.transform(train_data[di:di+smoothing_window_size,:])
 
-# You normalize the last bit of remaining data
+
 scaler.fit(train_data[di+smoothing_window_size:,:])
 train_data[di+smoothing_window_size:,:] = scaler.transform(train_data[di+smoothing_window_size:,:])
 
-# Reshape both train and test data
+
 train_data = train_data.reshape(-1)
 
-# Normalize test data
+
 test_data = scaler.transform(test_data).reshape(-1)
 
-# So the data will have a smoother curve than the original ragged data
+
 EMA = 0.0
 gamma = 0.1
 for ti in range(11000):
   EMA = gamma*train_data[ti] + (1-gamma)*EMA
   train_data[ti] = EMA
 
-# Used for visualization and test purposes
+
 all_mid_data = np.concatenate([train_data,test_data],axis=0)
 
 window_size = 100
