958e2dda6cabfabcf512f09a5804a66712968a40
mandar-01 <bhaleraomandar01@gmail.com>
2018-08-29 23:05:04 +0530

    dependencies

--- script.py
+++ script.py
@@ -1,3 +1,13 @@
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
 df = pd.read_csv(os.path.join('Stocks','hpq.us.txt'),delimiter=',',usecols=['Date','Open','High','Low','Close'])
 print('Loaded data from the Kaggle repository')
 
